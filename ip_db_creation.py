6be1d4234fe5a1b53d71047849c5e552c8c9c115
mirkontan <148462539+mirkontan@users.noreply.github.com>
2024-02-06 14:40:32 +0100

    Update ip_db_creation.py

--- ip_db_creation.py
+++ ip_db_creation.py
@@ -214,6 +214,7 @@ def process_xlsx_file(xlsx_file):
     trademarks_df_cn_rows['IPR_REG_NAME'] = trademarks_df_cn_rows['IPR_REG_NAME'].str.split(r'</div><div class="').str[1]
     trademarks_df_cn_rows['IPR_REG_NAME'] = trademarks_df_cn_rows['IPR_REG_NAME'].fillna("-")
     trademarks_df_cn_rows['IPR_REG_NAME'] = trademarks_df_cn_rows['IPR_REG_NAME'].str.split(r'">').str[1]
+    trademarks_df_cn_rows['IPR_REG_NAME'] = trademarks_df_cn_rows['IPR_REG_NAME'].fillna("-")
     trademarks_df_cn_rows['IPR_REG_NAME'] = trademarks_df_cn_rows['IPR_REG_NAME'].str.split(r'</div>').str[0]
 
     trademarks_df_cn_rows['IPR_NICE_CLASSES_ALL'] = trademarks_df_cn_rows['HTML'].str.split(r'商标分类').str[1]
