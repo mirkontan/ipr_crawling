2309509088daf6a0be89c316823c2ffb8c59b391
mirkontan <148462539+mirkontan@users.noreply.github.com>
2024-02-10 13:31:19 +0100

    Update ip_db_creation.py

--- ip_db_creation.py
+++ ip_db_creation.py
@@ -28,6 +28,13 @@ def process_xlsx_file(xlsx_file):
             elif row['IPR_JURISDICTION'] == 'GERMANY':
                 row['IPR_LINK_TO_ONLINE_DATABASE'] = f'https://register.dpma.de/DPMAregister/marke/registerhabm?AKZ={row["IPR_REGISTRATION_NUMBER"]}'
                 row['IPR_DATABASE_URL'] = row['IPR_LINK_TO_ONLINE_DATABASE']
+            elif row['IPR_JURISDICTION'] == 'UNITED KINGDOM':
+                iprregnum = row['IPR_REGISTRATION_NUMBER']
+                # Remove 'UK' from iprregnum if present
+                iprregnum = iprregnum.replace('UK', '')
+                row['IPR_LINK_TO_ONLINE_DATABASE'] = f'https://trademark-search.marcaria.com/en/result?number={iprregnum}&country=GB&classes=&status=1&mode=1&searchby=2'
+                row['IPR_DATABASE_URL'] = row['IPR_LINK_TO_ONLINE_DATABASE']
+
             elif row['IPR_JURISDICTION'] == 'UNITED STATES OF AMERICA' or row['IPR_JURISDICTION'] == 'UNITED STATES':
                 row['IPR_LINK_TO_ONLINE_DATABASE'] = f'https://branddb.wipo.int/en/quicksearch/brand/US5020060{row["IPR_REGISTRATION_NUMBER"]}'
                 row['IPR_DATABASE_URL'] = f'https://tsdr.uspto.gov/#caseNumber={row["IPR_REGISTRATION_NUMBER"]}&caseSearchType=US_APPLICATION&caseType=SERIAL_NO&searchType=statusSearch'
